71f309129a2f1a6aa9de9d948d62594f8197e9b0
Cristopher <cristopherpk@gmail.com>
2016-11-24 16:59:41 -0300

    adding user basic functions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,26 +6,62 @@ class UsersController < ApplicationController
 
   # POST /users/create
   def create_user
+    # TODO: Check for how to send the password thru params hash.
+    @user = User.new(username:           params[:username],
+                     first_name:         params[:first_name],
+                     last_name:          params[:last_name],
+                     email:              params[:email],
+                     encrypted_password: params[:encrypted_password])
+
+    # TODO: Verify user fields
+    if @user.save
+      redirect_to show_user_path(@user)
+    else
+      # TODO: If do not save user, redirect to an error page.
+      redirect_to root_path
+    end
   end
 
   # GET /users/:id/edit
   def edit_user
+    # TODO: Check if the user is logged into its own account, or if the user
+    # has admin privileges.
+    @user = User.find(params[:id])
   end
 
   # POST /users/:id/update
   def update_user
+    # TODO: Check if the user is logged into its own account.
+    @user = User.find(params[:id])
+
+    # TODO: Verify user fields
+    if @user.update(params)
+      redirect_to show_user_path(@user)
+    else
+      # TODO: If do not save user, redirect to an error page.
+      redirect_to root_path
   end
 
   # GET /users/:id
   def show_user
+    # TODO: Check if the user is logged into its own account or if it has admin
+    # privileges.
+    @user = User.find(params[:id])
   end
 
   # GET /users
+  # TODO: this controller must have admin privileges.
   def show_all_users
+    @users = User.all
   end
 
   # DELETE /users/:id
+  # TODO: this controller must have admin privileges.
   def delete_user
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      redirect_to show_all_users
   end
 
 end
--- db/migrate/20161115211028_devise_create_users.rb
+++ db/migrate/20161115211028_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## User application details
-      t.string :username
-      t.string :first_name
-      t.string :last_name
+      t.string :username, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -40,6 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
